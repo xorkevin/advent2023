fa419e1f19444d85c0f8225f42cac1521a574cf1
Kevin Wang <kevin@xorkevin.com>
2023-12-10 21:57:30 -0800

    Finish day11

---
+++ day11/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const (
+	puzzleInput = "input.txt"
+)
+
+func main() {
+	file, err := os.Open(puzzleInput)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	var emptyRows []int
+	var coords []Coord
+	var cols [][]byte
+
+	scanner := bufio.NewScanner(file)
+	for y := 0; scanner.Scan(); y++ {
+		isEmpty := true
+		line := scanner.Bytes()
+		if len(cols) == 0 {
+			cols = make([][]byte, len(line))
+		}
+		for x, i := range line {
+			if i == '#' {
+				isEmpty = false
+				coords = append(coords, Coord{
+					x: x,
+					y: y,
+				})
+				cols[x] = append(cols[x], i)
+			}
+		}
+		if isEmpty {
+			emptyRows = append(emptyRows, y)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	var emptyColumns []int
+	for x, i := range cols {
+		isEmpty := true
+		for _, c := range i {
+			if c == '#' {
+				isEmpty = false
+				break
+			}
+		}
+		if isEmpty {
+			emptyColumns = append(emptyColumns, x)
+		}
+	}
+
+	sum := 0
+	sum2 := 0
+	for n, i := range coords {
+		for _, j := range coords[n+1:] {
+			expansion := calcExpansion(emptyRows, i.y, j.y) + calcExpansion(emptyColumns, i.x, j.x)
+			dist := manhattanDistance(i, j)
+			sum += dist + expansion
+			sum2 += dist + expansion*999999
+		}
+	}
+
+	fmt.Println("Part 1:", sum)
+	fmt.Println("Part 2:", sum2)
+}
+
+func calcExpansion(emptyRows []int, a, b int) int {
+	if a > b {
+		a, b = b, a
+	}
+	left := 0
+	for left < len(emptyRows) && emptyRows[left] < a {
+		left++
+	}
+	right := len(emptyRows) - 1
+	for right >= 0 && emptyRows[right] > b {
+		right--
+	}
+	return right - left + 1
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func manhattanDistance(a, b Coord) int {
+	return abs(a.x-b.x) + abs(a.y-b.y)
+}
+
+type (
+	Coord struct {
+		x, y int
+	}
+)
+
+func isEmptyColumn(grid [][]byte, c int) bool {
+	for _, i := range grid {
+		if i[c] == '#' {
+			return false
+		}
+	}
+	return true
+}
